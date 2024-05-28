1dc9f94a72dd081294d2299b0baa9fad4a829c99
İlker Yasin AKSOY <16045948+bumb7ebee@users.noreply.github.com>
2024-05-28 04:35:59 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import requests as req
 
 WEATHER_REQUEST_URL = "https://api.openweathermap.org/data/2.5/weather?id={}&units={}&mod=json&appid={}"
 
-WEATHER_TEMPLATE="""<h3>Hello from <img src="https://github.com/bumb7ebee/bumb7ebee/blob/asset/images/icon/turkey_32.png" width="28" height="28"/> {}!</h3>
+WEATHER_TEMPLATE="""<h3>Hhello from <img src="https://github.com/bumb7ebee/bumb7ebee/blob/asset/images/icon/turkey_32.png" width="28" height="28"/> {}!</h3>
 <p>Currently, the weather is: <b> {}°{}, <img src="https://openweathermap.org/img/wn/{}.png" width="28" height="28" title= "Weather Icon" alt="Weather Icon"> <i>({})</i></b></br>Today, the sun rises at <b>{}</b> and sets at <b>{}</b>.</p>"""
 
 def refresh_contents(old_content, new_content):
